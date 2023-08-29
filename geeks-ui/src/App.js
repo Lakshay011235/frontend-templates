87437555b954d5e8babab021b3d6d57f57521cc0
Lakshay Sharma <107244796+Lakshay011235@users.noreply.github.com>
2023-08-30 02:36:06 +0530

    ok

--- geeks-ui/src/App.js
+++ geeks-ui/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.scss';
+import logo from './/images/logo.svg';
+import './/styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <img className="bi me-2" width="40" height="32" src={logo} />
+          </a>
+
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
+            <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
+            <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
+            <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
+            <li><a href="#" className="nav-link px-2 text-white">About</a></li>
+          </ul>
+
+          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search" />
+          </form>
+
+          <div className="text-end">
+            <button type="button" className="btn btn-outline-light me-2">Login</button>
+            <button type="button" className="btn btn-warning">Sign-up</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
